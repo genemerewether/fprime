b80eae600aa10b5b42c4db41e3b497c2193732a8
Gene Merewether <mereweth@jpl.nasa.gov>
2018-10-22 17:09:25 -0700

    Fix snapdragon sysroot path

--- mk/configs/host/Linux-quest.mk
+++ mk/configs/host/Linux-quest.mk
@@ -17,6 +17,8 @@ export HEXAGON_SDK_ROOT := /opt/tools/leo/Qualcomm/Hexagon_SDK/3.0
 export HEXAGON_TOOLS_ROOT := /opt/tools/leo/Qualcomm/HEXAGON_Tools/7.2.12/Tools
 export HEXAGON_ARM_SYSROOT := /opt/tools/leo/Qualcomm/qrlinux_sysroot
 
+export INDIGO_ARM_SYSROOT :=  /opt/tools/leo/Qualcomm/indigo_sysroot
+
 CRC := $(BUILD_ROOT)/mk/bin/run_file_crc.sh
 
 NM:= /usr/bin/nm
