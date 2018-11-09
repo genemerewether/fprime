eaadac0bb7c85b011b8109e70f6d31f23f598df4
Gene Merewether <mereweth@jpl.nasa.gov>
2018-11-09 10:11:30 -0800

    Allow EXTRA_LIBS in unit tests

--- HLProc/HLRosIface/test/ut/main.cpp
+++ HLProc/HLRosIface/test/ut/main.cpp
@@ -2,7 +2,17 @@
 // Main.cpp
 // ----------------------------------------------------------------------
 
+#include "Tester.hpp"
+#include <ros/ros.h>
+
 int main(int argc, char **argv) {
+  ros::init(argc,argv,"HLRosIface_ut");
+
+  ros::start();
+
+  //component.startPub();
+  //component.startIntTask(30, 20*1024);
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
