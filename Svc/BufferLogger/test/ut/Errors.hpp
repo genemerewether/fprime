66ba590800db1f33a83234fb302706cfe69bffdc
Gene Merewether <mereweth@jpl.nasa.gov>
2019-02-22 13:01:47 -0800

    Make BufferSend pass by reference, consistent with upstream FPrime

---
+++ Svc/BufferLogger/test/ut/Errors.hpp
@@ -0,0 +1,63 @@
+// ======================================================================
+// \title  Errors.hpp
+// \author bocchino, mereweth
+// \brief  Interface for BufferLogger error tests
+//
+// \copyright
+// Copyright (C) 2017 California Institute of Technology.
+// ALL RIGHTS RESERVED.  United States Government Sponsorship
+// acknowledged. Any commercial use must be negotiated with the Office
+// of Technology Transfer at the California Institute of Technology.
+//
+// This software may be subject to U.S. export control laws and
+// regulations.  By accepting this document, the user agrees to comply
+// with all U.S. export laws and regulations.  User has the
+// responsibility to obtain export licenses, or other export authority
+// as may be required before exporting such information to foreign
+// countries or providing access to foreign persons.
+// ======================================================================
+
+#ifndef Svc_Errors_HPP
+#define Svc_Errors_HPP
+
+#include "Tester.hpp"
+
+namespace Svc {
+
+  namespace Errors {
+
+    class Tester :
+      public Svc::Tester
+    {
+
+      public:
+
+        //! Construct object Tester
+        //!
+        Tester(
+            BufferLoggerFileMode writeMode = BL_REGULAR_WRITE,  //! The write mode - direct, bulk, or looping
+            BufferLoggerCloseMode closeMode = BL_CLOSE_SYNC,
+            U32 directChunkSize = 512, //! The filesystem chunk size - direct mode uses this
+            bool doInitLog = true
+        );
+
+        // ----------------------------------------------------------------------
+        // Tests
+        // ----------------------------------------------------------------------
+
+        //! Log file open error
+        void LogFileOpen(void);
+
+        //! Log file write error
+        void LogFileWrite(void);
+
+        //! Log file validation error
+        void LogFileValidation(void);
+
+    };
+
+  }
+
+}
+
+#endif
