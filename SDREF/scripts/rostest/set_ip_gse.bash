25a3a517efff57746d8252c6a914bd4ab72dd899
Gene Merewether <mereweth@jpl.nasa.gov>
2019-05-02 22:15:58 -0700

    updates to scripts

--- SDREF/scripts/rostest/set_ip_gse.bash
+++ SDREF/scripts/rostest/set_ip_gse.bash
@@ -1,6 +1,2 @@
-#export ROS_IP=192.168.1.118
-#export ROS_HOSTNAME=${ROS_IP}
-#export ROS_MASTER_URI="http://192.168.1.5:11311"
-
 export ROS_IP=192.168.1.2
 export ROS_MASTER_URI="http://192.168.1.1:11311"
