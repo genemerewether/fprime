27eea1e08270f38bb933ae07444311a03ae24ad1
Gene Merewether <mereweth@jpl.nasa.gov>
2019-05-06 10:57:49 -0700

    add CARREF for ground vehicles with ackermann steering

---
+++ CARREF/Top/mod.mk
@@ -0,0 +1,12 @@
+SRC = 				Topology.cpp \
+                    CARREFTopologyAppAi.xml
+
+SRC_DSPAL = 		StubTargetInit.cpp
+
+SRC_SDFLIGHT = 		StubTargetInit.cpp
+
+SRC_CYGWIN = 		StubTargetInit.cpp
+
+SRC_DARWIN =        StubTargetInit.cpp
+
+SRC_LINUX =         StubTargetInit.cpp
---
+++ CARREF/runCARREFgdb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# check that time is sufficiently recent
+if [ $(date +%s) -ge 1509000000 ]; then
+    echo "time is after October 25, 2017 - manual sanity check"
+else
+    echo "time is too old - refusing to start"
+    exit
+fi
+
+# start SW in screen so we can detach and reattach
+# start SW in script so we can record interactive session
+screen -S "CARREF" -d -m bash --norc
+screen -S "CARREF" -X logfile $(date +"/eng/CARREF_%F_%H-%M.log")
+screen -S "CARREF" -X log
+screen -r "CARREF" -X stuff "export ROS_HOME=/eng/ros\n"
+screen -r "CARREF" -X stuff ". /eng/load/set_ip_snap.bash || true\n"
+screen -r "CARREF" -X stuff ". /opt/ros/indigo/setup.bash || true\n"
+screen -r "CARREF" -X stuff "pgrep roscore || roscore &\n"
+screen -r "CARREF" -X stuff "gdb /golden/CARREF0\n"
+screen -r "CARREF" -X stuff "handle SIGILL pass noprint nostop\n"
+screen -r "CARREF" -X stuff "r -p 50100 -u 50020 -z localhost:50030 -a localhost -s"
+screen -r "CARREF"
