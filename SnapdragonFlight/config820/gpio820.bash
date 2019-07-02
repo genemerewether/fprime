540b80e517de1a00cf8feb86377ed4005f2abd2e
Gene Merewether <mereweth@jpl.nasa.gov>
2019-07-02 16:42:44 -0700

    can toggle pin 84

--- SnapdragonFlight/config820/gpio820.bash
+++ SnapdragonFlight/config820/gpio820.bash
@@ -13,6 +13,7 @@ gpio_high() {
 gpio_high 34  # EXT_IMU_I2C_MUX_SEL
 gpio_high 73  # SONAR_UART_MUX_SEL
 gpio_high 77  # IMU1_SPI_MUX_SEL
+gpio_high 117 # SPEKTRUM_PWR_EN
 #gpio_high 97  # EAGLENEST_UART_MUX_SEL
 gpio_high 110 # ESC_UART_MUX_SEL
 gpio_high 115 # BAROM_I2C_MUX_SEL
