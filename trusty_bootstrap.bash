77eb3170778798af3543efefaa343fe5bb20f9e6
Gene Merewether <mereweth@jpl.nasa.gov>
2019-07-19 11:21:50 -0700

    add ewok deps to trusty 801 bootstrap

--- trusty_bootstrap.bash
+++ trusty_bootstrap.bash
@@ -11,12 +11,13 @@ locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 apt-get update
 
 apt-get install -y ros-indigo-ros-base ros-indigo-image-transport \
-    ros-indigo-trac-ik ros-indigo-orocos-kdl \
+        ros-indigo-trac-ik ros-indigo-orocos-kdl \
+        ros-indigo-sophus libsuitesparse-dev \
     libeigen3-dev libopencv-dev \
     git software-properties-common python-pip \
     symlinks
