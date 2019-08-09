71455300f6fb46a7970a9e83a2d69e24d798cdd9
Gene Merewether <mereweth@jpl.nasa.gov>
2019-08-09 09:44:23 -0700

    Add QualcommESC to HEXREF

--- HEXREF/Top/Topology.cpp
+++ HEXREF/Top/Topology.cpp
@@ -124,6 +124,7 @@ void allocComps() {
         0, // mixer
         Gnc::ACTADAP_SCHED_CONTEXT_TLM, // adapter
         0, // actCtrl
+        0, // qcEsc
         0, // logQueue
         0, // chanTlm
         0, // prmDb
